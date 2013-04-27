bd3f7466563646d55b06adffcb3a0af54d3ff1dc
Tom Aldcroft <taldcroft@gmail.com>
2013-04-26 21:29:53 -0400

    Update skare version to 0.16dev

--- ska_version.py
+++ ska_version.py
@@ -9,7 +9,7 @@ NOTE: this code copied from astropy.version and simplified.  Any license
 restrictions therein are applicable.
 """
 
-version = '0.15'
+version = '0.16dev'
 
 
 def _get_git_devstr():
