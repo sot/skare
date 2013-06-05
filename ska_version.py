6406f68a0b5bad93f9aeabf5e8036d6837effa40
Jean Connelly <jconnelly@cfa.harvard.edu>
2013-06-05 12:31:30 -0300

    Update ska_version to 0.17dev

--- ska_version.py
+++ ska_version.py
@@ -9,7 +9,7 @@ NOTE: this code copied from astropy.version and simplified.  Any license
 restrictions therein are applicable.
 """
 
-version = '0.16'
+version = '0.17dev'
 
 
 def _get_git_devstr():
