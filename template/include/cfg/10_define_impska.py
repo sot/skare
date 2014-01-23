def9ba37c5f227acaeb96998cf325076155a8231
Tom Aldcroft <taldcroft@gmail.com>
2014-01-23 14:18:44 -0500

    Add kadi.events and astropy.table.Table to %impska magic imports

--- template/include/cfg/10_define_impska.py
+++ template/include/cfg/10_define_impska.py
@@ -6,5 +6,7 @@ def import_ska(self, arg):
     ip.ex('import Ska.engarchive.fetch_eng as fetch')
     ip.ex('from Chandra.Time import DateTime')
     ip.ex('from Ska.Matplotlib import plot_cxctime, cxctime2plotdate')
+    ip.ex('from kadi import events')
+    ip.ex('from astropy.table import Table')
 
 ip.define_magic('impska', import_ska)
