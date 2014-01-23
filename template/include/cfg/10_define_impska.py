68690a902cfbaedb75833cb09e2e60eb32d3548c
Tom Aldcroft <taldcroft@gmail.com>
2014-01-23 17:52:20 -0500

    Add matplotlib and numpy imports to impska as well

--- template/include/cfg/10_define_impska.py
+++ template/include/cfg/10_define_impska.py
@@ -8,5 +8,7 @@ def import_ska(self, arg):
     ip.ex('from Ska.Matplotlib import plot_cxctime, cxctime2plotdate')
     ip.ex('from kadi import events')
     ip.ex('from astropy.table import Table')
+    ip.ex('import numpy as np')
+    ip.ex('import matplotlib.pyplot as plt')
 
 ip.define_magic('impska', import_ska)
